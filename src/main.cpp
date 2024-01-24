1495c6a1544f5e395721b063f7c16f575683198c
Alexander Vance <perphontibecu@gmx.net>
2024-01-24 10:30:27 -0500

    CI: Clean up C++ data loader based on user feedback.

--- src/main.cpp
+++ src/main.cpp
@@ -420,3 +420,5 @@ int main(int argc, char* argv[]) {
 # Simulated change for commit 14 on 2024-01-19 13:36:07
 
 # Simulated change for commit 17 on 2024-01-23 14:44:19
+
+# Simulated change for commit 18 on 2024-01-24 10:30:27
