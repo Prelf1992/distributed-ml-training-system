88b878206497eea29d522fcf995594d246148a17
Alexander Vance <perphontibecu@gmx.net>
2024-01-26 11:56:36 -0500

    CI: Update build config for C++ CV module for better readability.

--- src/main.cpp
+++ src/main.cpp
@@ -426,3 +426,5 @@ int main(int argc, char* argv[]) {
 # Simulated change for commit 19 on 2024-01-25 17:48:49
 
 # Simulated change for commit 20 on 2024-01-26 11:26:58
+
+# Simulated change for commit 21 on 2024-01-26 11:56:36
