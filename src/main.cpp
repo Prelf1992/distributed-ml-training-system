21cb325c55ac34d6cf1dea83b47995bcac6137df
Alexander Vance <perphontibecu@gmx.net>
2024-04-18 10:47:56 -0400

    Feat: Update build config for C++ LLM component to support new requirements.

--- src/main.cpp
+++ src/main.cpp
@@ -474,3 +474,5 @@ int main(int argc, char* argv[]) {
 # Simulated change for commit 68 on 2024-04-16 17:29:16
 
 # Simulated change for commit 69 on 2024-04-16 15:59:34
+
+# Simulated change for commit 70 on 2024-04-18 10:47:56
